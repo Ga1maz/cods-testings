7091d5793b5adb4fa617deecab2d6271e5f3306f
Алмаз <120911287+Ga1maz@users.noreply.github.com>
2024-12-30 05:29:15 +0500

    Update gps.ino

--- gps.ino
+++ gps.ino
@@ -1,65 +1,66 @@
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-#include <SoftwareSerial.h>
-#include <TinyGPS++.h>
+#include <ESP8266WiFi.h>         // Подключение библиотеки для работы с Wi-Fi
+#include <PubSubClient.h>         // Подключение библиотеки для работы с MQTT
+#include <SoftwareSerial.h>       // Подключение библиотеки для программного последовательного порта
+#include <TinyGPS++.h>            // Подключение библиотеки для работы с GPS
 
-//Указать данные mqtt, и wifi сети
-const char* ssid = "***********";
-const char* password = "*****";
-const char* mqtt_server = "**********";
-const char* mqtt_user = "*****";
-const char* mqtt_password = "**********";
+// Указать данные для подключения к Wi-Fi и MQTT
+const char* ssid = "***********";               // SSID Wi-Fi сети
+const char* password = "*****";                  // Пароль Wi-Fi сети
+const char* mqtt_server = "**********";          // Адрес MQTT сервера
+const char* mqtt_user = "*****";                 // Имя пользователя для MQTT
+const char* mqtt_password = "**********";        // Пароль для MQTT
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-TinyGPSPlus gps;
-SoftwareSerial GPSSerial(4, 5); // RX (D2), TX (D1)
+WiFiClient espClient;                            // Объект для подключения по Wi-Fi
+PubSubClient client(espClient);                  // Объект для работы с MQTT
+TinyGPSPlus gps;                                // Объект для работы с GPS
+SoftwareSerial GPSSerial(4, 5);                  // Настройка программного последовательного порта (RX: D2, TX: D1)
 
-unsigned long lastSendTime = 0; 
-const unsigned long sendInterval = 500; 
+unsigned long lastSendTime = 0;                  // Время последней отправки координат
+const unsigned long sendInterval = 500;          // Интервал между отправками координат (в миллисекундах)
 
 void setup() {
-    Serial.begin(115200);
-    GPSSerial.begin(9600);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
+    Serial.begin(115200);                        // Инициализация серийного порта для отладки
+    GPSSerial.begin(9600);                       // Инициализация GPS-модуля (9600 бод)
+    WiFi.begin(ssid, password);                  // Подключение к Wi-Fi сети
+    while (WiFi.status() != WL_CONNECTED) {      // Ожидание подключения к Wi-Fi
         delay(500);
         Serial.print(".");
     }
-    Serial.println("Connected to WiFi");
-    client.setServer(mqtt_server, 1883);
+    Serial.println("Connected to WiFi");         // Выводим сообщение о подключении к Wi-Fi
+    client.setServer(mqtt_server, 1883);         // Настройка MQTT сервера (порт 1883)
 }
 
 void reconnect() {
-    while (!client.connected()) {
-        if (client.connect("ESP8266GPS", mqtt_user, mqtt_password)) {
-            Serial.println("Connected to MQTT");
+    while (!client.connected()) {                // Пока не подключены к MQTT серверу
+        if (client.connect("ESP8266GPS", mqtt_user, mqtt_password)) {   // Подключаемся с заданными учетными данными
+            Serial.println("Connected to MQTT");  // Сообщение о подключении к MQTT
         } else {
-            delay(5000);
+            delay(5000);                         // Ожидание 5 секунд перед повторной попыткой
         }
     }
 }
 
 void loop() {
-    if (!client.connected()) {
-        reconnect();
+    if (!client.connected()) {                   // Если MQTT-соединение не установлено
+        reconnect();                              // Переподключаемся
     }
 
-    client.loop();
-    while (GPSSerial.available() > 0) {
-        gps.encode(GPSSerial.read());
+    client.loop();                               // Обработка входящих сообщений MQTT
+    while (GPSSerial.available() > 0) {           // Пока есть данные от GPS
+        gps.encode(GPSSerial.read());             // Обработка данных GPS
     }
-    //получение и отправка кординат
+
+    // Проверка на обновление координат GPS
     if (gps.location.isUpdated() && gps.location.isValid()) {
-        unsigned long currentMillis = millis();
-        if (currentMillis - lastSendTime >= sendInterval) {
-            lastSendTime = currentMillis; 
-            float latitude = gps.location.lat();
-            float longitude = gps.location.lng();
-            String coordinates = String(latitude, 6) + "," + String(longitude, 6);
-            client.publish("gps/coordinates", coordinates.c_str());
-            Serial.println("Sent: " + coordinates);
+        unsigned long currentMillis = millis();  // Получаем текущее время в миллисекундах
+        if (currentMillis - lastSendTime >= sendInterval) {   // Если прошло достаточно времени для отправки
+            lastSendTime = currentMillis;         // Обновляем время последней отправки
+            float latitude = gps.location.lat();  // Получаем широту
+            float longitude = gps.location.lng(); // Получаем долготу
+            String coordinates = String(latitude, 6) + "," + String(longitude, 6); // Формируем строку с координатами
+            client.publish("gps/coordinates", coordinates.c_str()); // Отправка координат в топик MQTT
+            Serial.println("Sent: " + coordinates);  // Выводим координаты в консоль для отладки
         }
     }
-    delay(10); 
+    delay(10);  // Небольшая задержка для предотвращения излишней загрузки процессора
 }
