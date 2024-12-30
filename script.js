326349da08fd72ceff9433c652d1effccf5c76d5
Алмаз <120911287+Ga1maz@users.noreply.github.com>
2024-12-30 07:25:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ infoPanel.update = function (coords1 = '', coords2 = '', distance = '') {
 infoPanel.addTo(map);
 
 // Подключение к MQTT через защищенное соединение WebSocket
-const client = mqtt.connect('ws://mqtt.cloa.su:8883', {
+const client = mqtt.connect('wss://mqtt.cloa.su:8883', {
     username: 'ga1maz',
     password: 'almazg1234'
 });
