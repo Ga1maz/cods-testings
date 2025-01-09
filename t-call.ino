fcfa3f0507eb8d2e48e0cb6d29bd864b96914a88
Алмаз <120911287+Ga1maz@users.noreply.github.com>
2025-01-10 00:39:38 +0500

    Update t-call.ino

--- t-call.ino
+++ t-call.ino
@@ -1,123 +1,233 @@
-#define PIN_TRIGGER 5  // Пин для проверки 5V
-
-// Определение версии SIM800L модема
-#define SIM800L_IP5306_VERSION_20190610
-// #define SIM800L_AXP192_VERSION_20200327
-// #define SIM800C_AXP192_VERSION_20200609
-// #define SIM800L_IP5306_VERSION_20200811
-
-// Определение серийного порта для отладки
-#define DUMP_AT_COMMANDS
-#define TINY_GSM_DEBUG          SerialMon
-
-#include "utilities.h"  // Подключение библиотеки для утилит (если есть)
-
-// Настройка серийного порта для вывода отладочных данных (по умолчанию 115200)
-#define SerialMon Serial
-// Настройка серийного порта для отправки AT-команд модему
-#define SerialAT  Serial1
-
-// Настройка библиотеки TinyGSM
-#define TINY_GSM_MODEM_SIM800          // Используем модем SIM800
-#define TINY_GSM_RX_BUFFER      1024   // Размер буфера RX — 1Кб
-
-#include <TinyGsmClient.h>  // Подключение библиотеки для работы с модемом SIM800
-
-// Определяем использование StreamDebugger для отладки AT-команд
-#ifdef DUMP_AT_COMMANDS
-#include <StreamDebugger.h>
-StreamDebugger debugger(SerialAT, SerialMon);  // Создание объекта StreamDebugger
-TinyGsm modem(debugger);  // Создание объекта модема с отладчиком
-#else
-TinyGsm modem(SerialAT);  // Создание объекта модема без отладчика
-#endif
-
-// Определение номеров телефонов для тестирования SMS и вызовов
-#define SMS_TARGET  "+79631413596"   // Номер для отправки SMS
-#define CALL_TARGET "+79631413596"   // Номер для вызова
-
-void setup()
-{
-    // Устанавливаем скорость обмена для консольного порта
-    SerialMon.begin(115200);
-
-    delay(10);  // Короткая задержка для стабилизации
-
-    // Запуск управления питанием
-    if (setupPMU() == false) {
-        Serial.println("Setting power error");  // Сообщение об ошибке при настройке питания
-    }
-
-    // Выполнение начальной настройки модема
-    setupModem();
-
-    // Настройка порта для AT-команд модема
-    SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
-
-    // Инициализация пина 5 как входа
-    pinMode(PIN_TRIGGER, INPUT);
-
-    delay(6000);  // Задержка перед началом работы
+html,
+body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
 }
 
-void loop()
-{
-    // Проверка, есть ли на пине 5 напряжение 5V (логическая "высокая" величина)
-    if (digitalRead(PIN_TRIGGER) == HIGH) {
-        SerialMon.println("Pin 5 is HIGH, proceeding with modem operations...");  // Выводим сообщение, если пин 5 высокий
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+    height: 100vh;
+    background: url('bak.jpg') no-repeat center center fixed;
+    background-size: cover;
+    overflow: hidden;
+}
 
-        // Модем может перезапускаться долго, для пропуска перезапуска можно вызвать init() вместо restart()
-        SerialMon.println("Initializing modem...");
-        modem.restart();  // Перезапуск модема
+.container {
+    background: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(10px);
+    border-radius: 15px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    margin: 10px;
+    overflow: hidden;
+    height: calc(100vh - 93px);
+    display: flex;
+    flex-direction: column;
+    opacity: 0;
+    animation: slideUp 1s forwards;
+}
 
-        // Переключение аудио каналов
-        SerialAT.print("AT+CHFA=1\r\n");
-        delay(2);
+.container>* {
+    opacity: 0;
+    animation: fadeInElement 1s forwards;
+}
 
-        // Установка уровня звука звонка
-        SerialAT.print("AT+CRSL=100\r\n");
-        delay(2);
+.container>*:nth-child(1) {
+    animation-delay: 1s;
+}
 
-        // Установка громкости динамика
-        SerialAT.print("AT+CLVL=100\r\n");
-        delay(2);
+.container>*:nth-child(2) {
+    animation-delay: 1.2s;
+}
 
-        // Включение представления номера вызывающего абонента
-        SerialAT.print("AT+CLIP=1\r\n");
-        delay(10000);  // Задержка для выполнения команды
+.container>*:nth-child(3) {
+    animation-delay: 1.4s;
+}
 
-        // Выводим номер, на который будем звонить
-        Serial.print("Calling:");
-        Serial.println(CALL_TARGET);
+.container>*:nth-child(4) {
+    animation-delay: 1.6s;
+}
 
-        // Выполняем звонок
-        bool res = modem.callNumber(CALL_TARGET);
+.map-container {
+    flex: 1;
+    max-width: 30%;
+}
 
-        // Выводим результат звонка
-        Serial.print("Call:");
-        Serial.println(res ? "OK" : "fail");
+.status-container {
+    flex: 1;
+    max-width: 40%;
+}
 
-        // Инициализация модема после звонка
-        SerialMon.println("Initializing modem...");
-        modem.restart();
+.graph-container {
+    flex: 1;
+    max-width: 30%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
 
-        delay(10000);  // Задержка для стабилизации
+#map {
+    width: 100%;
+    height: 300px;
+    border-radius: 10px;
+    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.5);
+}
 
-        // Получаем IMEI модема
-        String imei = modem.getIMEI();
-        DBG("IMEI:", imei);
+.chart {
+    flex: 1;
+    background: rgba(255, 255, 255, 0.05);
+    padding: 15px;
+    border-radius: 15px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
 
-        // Отправляем SMS с IMEI
-        bool smsRes = modem.sendSMS(SMS_TARGET, String("Hello from ") + imei);
-        DBG("SMS:", smsRes ? "OK" : "fail");
+.data-box {
+    margin: 10px 0;
+    padding: 15px;
+    border-radius: 10px;
+    background-color: rgba(255, 255, 255, 0.021);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    font-size: 18px;
+    white-space: nowrap;
+    opacity: 0;
+    animation: fadeIn 1s forwards;
+}
 
-        // Задержка перед следующей проверкой пина
-        delay(10000);
-    } else {
-        // Если пин 5 низкий, пропускаем операции с модемом
-        SerialMon.println("Pin 5 is LOW, skipping modem operations...");
+.battery-box {
+    margin: 10px 0;
+    padding: 15px;
+    border-radius: 10px;
+    text-align: center;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.leaflet-control-attribution {
+    display: none;
+}
+
+.gps-info {
+    margin-top: 15px;
+}
+
+.loading-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+}
+
+.loading-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+}
+
+.loading-container .dot {
+    width: 40px;
+    height: 40px;
+    background-color: black;
+    border-radius: 50%;
+    animation: loading 1s infinite alternate;
+}
+
+.loading-container .dot:nth-child(1) {
+    background-color: #4285f4;
+    animation-delay: -0.25s;
+}
+
+.loading-container .dot:nth-child(2) {
+    background-color: #ea4335;
+    animation-delay: -0.5s;
+}
+
+.loading-container .dot:nth-child(3) {
+    background-color: #fbbc05;
+    animation-delay: -0.75s;
+}
+
+.loading-container .dot:nth-child(4) {
+    background-color: #34a853;
+    animation-delay: -1s;
+}
+
+@keyframes loading {
+    0% {
+        transform: translateY(-15px);
     }
 
-    // Модем поддерживает работу в фоновом режиме
-    modem.maintain();  // Поддержание соединения с модемом
+    100% {
+        transform: translateY(5px);
+    }
+}
+
+@keyframes fadeIn {
+    to {
+        opacity: 1;
+    }
+}
+
+@keyframes fadeInElement {
+    from {
+        opacity: 0;
+        transform: translateY(20px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+@keyframes slideUp {
+    from {
+        opacity: 0;
+        transform: translateY(100px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+.mobile-warning {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    color: white;
+    display: none;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    z-index: 10000;
+    text-align: center;
+}
+
+.mobile-warning .icon {
+    color: red;
+    font-size: 50px;
+    margin-bottom: 20px;
 }
